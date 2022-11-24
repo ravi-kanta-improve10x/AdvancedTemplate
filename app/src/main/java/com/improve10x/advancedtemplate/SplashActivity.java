a52961e2487fa9dd86b0562e8291cefa43b39e73
Ravikumar <ravi.kanta@improve10x.com>
2022-11-24 11:37:17 +0530

    create Splash Activity

---
+++ app/src/main/java/com/improve10x/advancedtemplate/SplashActivity.java
@@ -0,0 +1,22 @@
+package com.improve10x.advancedtemplate;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        getSupportActionBar().hide();
+        Handler handler = new Handler();
+        handler.postDelayed(() ->{
+            Intent intent = new Intent(this,TemplateActivity.class);
+        },3000 );
+
+    }
+}
